db9ebf45e18faa48e2e2e951c258d417d889af0a
527mbeard <527mbeard@frhsd.com>
2026-01-08 09:32:08 -0500

    some js

---
+++ script.js
@@ -0,0 +1,32 @@
+{if (movie === 'The Grey Man') {
+  prefix="Agent";
+} else if(movie === 'The Beekeeper') {
+  prefix="Sergeant";
+} else if(movie === 'Fast and Furious:Hobbs and Shaw') {
+  prefix="Captain";
+} else if(movie === 'The Gorge') {
+  prefix="Officer";
+} else if(movie === 'The Running Man') {
+  prefix="General";
+} else if(movie === 'The Amateur') {
+  prefix="Private";
+} else if(movie === 'Heads of State') {
+  prefix="Commander";
+} else if(movie === 'Ghosted') {
+  prefix="Major";
+} 
+return prefix
+}
+    
+switch (color ) {
+    case 'red': middleName('crazy'); break;
+    case 'orange': middleName('sneaky'); break;
+    case 'yellow': middleName('fast'); break;
+    case 'green': middleName('quiet'); break;
+    case 'blue': middleName('dark'); break;
+    case 'purple': middleName('mysterious'); break;
+    case 'pink': middleName('hidden'); break;
+    case 'black': middleName('secret'); break;
+    case 'brown': middleName('sinister'); break;
+}
+ return middle;
