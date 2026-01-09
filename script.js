31deb0e066cd186447c4433569e49ca8559ab007
Madison Beard <527mbeard@frhsd.com>
2026-01-09 15:11:56 +0000

    all functions

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-{if (movie === 'The Grey Man') {
+function getPrefix{
+  if (movie === 'The Grey Man') {
   prefix="Agent";
 } else if(movie === 'The Beekeeper') {
   prefix="Sergeant";
@@ -16,9 +17,26 @@
   prefix="Major";
 } 
 return prefix
-}
+} 
+
+function getFirstName(){
+  if (letter === 'a-e') {
+  name="Archer";
+} else if(letter === 'f-i') {
+  name="falcon";
+} else if(letter === 'j-n') {
+  prefix="jones";
+} else if(letter === 'o-r') {
+  prefix="orlando";
+} else if(letter === 's-z') {
+  prefix="sky";
+} 
+return firstletter
+} 
+ 
     
-switch (color ) {
+function getMiddlename(){
+  switch (color ) {
     case 'red': middleName('crazy'); break;
     case 'orange': middleName('sneaky'); break;
     case 'yellow': middleName('fast'); break;
@@ -28,5 +46,57 @@ switch (color ) {
     case 'pink': middleName('hidden'); break;
     case 'black': middleName('secret'); break;
     case 'brown': middleName('sinister'); break;
-}
+}}
  return middle;
+
+ function getLastname(){
+  if (actor === 'Glen Powell') {
+  name="Pheonix";
+} else if(actor === 'Miles Teller') {
+  name="Shadow";
+} else if(actor === 'Anya Taylor Joy') {
+  prefix="Powers";
+} else if(actor === 'Jason Statham') {
+  prefix="Hunter";
+} else if(actor === 'John Cena') {
+  prefix="Scorpian";
+} else if(actor === 'Chris Evans') {
+  prefix="Star";
+} else if(actor === 'Ryan Gosling') {
+  prefix="Metal";
+} else if(actor === 'Idris Elba') {
+  prefix="Echo";
+} else if(actor === 'Ana De Amras') {
+  prefix="Night";
+} 
+return Middlename
+} 
+ 
+function getSuffix(){
+  if (birth === 'January') {
+  name="Jr.";
+} else if(birth === 'February') {
+  name="Sr.";
+}else if(birth === 'March') {
+  name="PhD";
+}else if(birth === 'April') {
+  name="MD";
+}else if(birth === 'May') {
+  name="Jr.";
+}else if(birth === 'June') {
+  name="Sr.";
+}else if(birth === 'July') {
+  name="PhD";
+}else if(birth === 'August') {
+  name="MD";
+}else if(birth === 'September') {
+  name="Jr.";
+}else if(birth === 'October') {
+  name="Sr.";
+}else if(birth === 'November') {
+  name="PhD";
+}else if(birth === 'December') {
+  name="MD";
+}
+  return birthday
+}
